d1642efb1fac826e035668782902b66ada9d4049
Mirza Abazovic <mirza.abazovic@gmail.com>
2015-04-09 14:06:55 +0200

    Added route that takes parameter //Add route that takes parameter //GET /{name}server.route({    method: 'GET',    path: '/{name}',    handler: function (request, reply) {        reply('Hello ' + encodeURIComponent(request.params.name));    }});

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var Hapi = require('hapi');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
+
 server.connection({ 
     host: 'localhost', 
     port: 8000 
 });
 
 // Add the route
+//GET /hello
 server.route({
     method: 'GET',
     path:'/hello', 
@@ -16,5 +18,18 @@ server.route({
     }
 });
 
+//Add route that takes parameter 
+//GET /{name}
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello ' + encodeURIComponent(request.params.name));
+    }
+});
+
+
 // Start the server
-server.start();
+server.start(function () {
+    console.log('Server running at :', server.info.uri);
+});
